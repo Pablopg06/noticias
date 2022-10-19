ca4195ebc90cea68f6d11166136d250d4b62459f
Pablopg06 <pablopg0694@gmail.com>
2022-10-19 09:29:19 -0300

    Segundo commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'noticias/index';
+    public const HOME = 'noticias';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
